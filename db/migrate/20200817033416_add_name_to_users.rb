59feb44c14695315f8ca42950650d256bb7f84c7
takamoto0609 <takamoto0609@gamil.com>
2020-08-17 12:37:41 +0900

    usersテーブルにカラムを追加

---
+++ db/migrate/20200817033416_add_name_to_users.rb
@@ -0,0 +1,10 @@
+class AddNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password, :integer
+    add_column :users, :first_name_kannji, :string
+    add_column :users, :last_name_kannji, :string
+    add_column :users, :first_name_katakana, :string
+    add_column :users, :last_name_katakana, :string
+    add_column :users, :birth_day, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_190032) do
+ActiveRecord::Schema.define(version: 2020_08_17_033416) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_08_15_190032) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "password"
+    t.string "first_name_kannji"
+    t.string "last_name_kannji"
+    t.string "first_name_katakana"
+    t.string "last_name_katakana"
+    t.integer "birth_day"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
