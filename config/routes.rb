eb35cf758c175af2321429007ebaf3d8a52db4b9
takamoto0609 <takamoto0609@gamil.com>
2020-08-20 19:43:50 +0900

    TOPページを整形
    必要なCSSファイルを配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new] # indexとnewアクションができるように
   resources :users, only: [:new, :create]
 end
