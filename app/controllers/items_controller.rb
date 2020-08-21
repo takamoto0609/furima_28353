ea19dd4d781994d043b80f7bebcac5acb3611695
takamoto0609 <takamoto0609@gamil.com>
2020-08-21 19:00:14 +0900

    createアクションを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def create # new.html.erbの「出品する」ボタンに対応するアクション
+  def create # new.html.erbの出品確定ボタンに対応するアクション
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :user_id, :category_id, :status_id, :shipping_fee_burden_id, :shipping_area_id, :estimated_delivery_date_id)
   end
 
 end
