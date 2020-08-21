8814a38f6a883139b0bcf7518f72ec94bf9de05e
takamoto0609 <takamoto0609@gamil.com>
2020-08-21 11:45:40 +0900

    出品画面に遷移できるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,11 @@ class ItemsController < ApplicationController
     @items = Item.all #Itemテーブルのデータ全てを@itemsに代入
   end
 
-  def new
+  def new #トップページの「出品」ボタンに対応するアクション
     @item = Item.new
+  end
+
+  def create # new.html.erbの「出品する」ボタンに対応するアクション
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
-  resources :items, only: [:index, :new] # indexとnewアクションができるように
+  resources :items, only: [:index, :new] # indexとnewアクションができる
   resources :users, only: [:new, :create]
 end
