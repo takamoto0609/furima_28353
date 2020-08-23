6552263521d13893e8dbde7ee483823db8e686ad
takamoto0609 <takamoto0609@gamil.com>
2020-08-24 01:37:46 +0900

    ユーザーモデル単体テストの不具合を解消

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     last_name_kannji{Faker::Japanese::Name.last_name}
     first_name_katakana{Faker::Japanese::Name.first_name.yomi}
     last_name_katakana{Faker::Japanese::Name.last_name.yomi}
-    birth_day{Faker::Number.number(digits: 8)}
+    birth_day{Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
