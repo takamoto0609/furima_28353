ab6b5f8d6b9e03d4e0f77af257b43fea6b561289
takamoto0609 <takamoto0609@gamil.com>
2020-08-24 15:13:21 +0900

    不要なコメントアウトを削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,11 +1,8 @@
 FactoryBot.define do
   factory :item do
-    # name                         {Faker::String.random(length: 1..40)}
     name                         {Faker::Name.name}
     price                        {Faker::Number.between(from: 300, to: 9999999)}
-    # explanation                  {Faker::String.random(length: 1..1000)}
     explanation                  {Faker::Name.name}
-    # user_id                      {Faker::Number.between(from: 1, to: 1)}
     category_id                  {Faker::Number.between(from: 2, to: 11)}
     status_id                    {Faker::Number.between(from: 2, to: 7)}
     shipping_fee_burden_id       {Faker::Number.between(from: 2, to: 3)}
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,61 +15,51 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品がうまくいかないとき' do
-      # 画像は1枚必須であること(ActiveStorageを使用すること)
       it "Imageが空だと出品できない" do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      # 商品名が必須であること
       it "商品名が空だと出品できない" do
         @item.name = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      # 商品の説明が必須であること
       it "商品の説明が空だと出品できない" do
         @item.explanation = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      # カテゴリーの情報が必須であること
       it "カテゴリーの情報が空だと出品できない" do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category Select")
       end
-      # 商品の状態についての情報が必須であること
       it "商品の状態についての情報が空だと出品できない" do
         @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status Select")
       end
-      # 配送料の負担についての情報が必須であること
       it "配送料の負担についての情報が空だと出品できない" do
         @item.shipping_fee_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee burden Select")
       end
-      # 発送元の地域についての情報が必須であること
       it "発送元の地域についての情報が空だと出品できない" do
         @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area Select")
       end
-      # 発送までの日数についての情報が必須であること
       it "発送までの日数についての情報が空だと出品できない" do
         @item.estimated_delivery_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Estimated delivery date Select")
       end
-      # 価格についての情報が必須であること
       it "発送元の地域についての情報が空だと出品できない" do
         @item.price = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      # 価格の範囲が、¥300~¥9,999,999の間であること
       it "価格の範囲が、¥300以上でないと出品できない" do
         @item.price = "200"
         @item.valid?
