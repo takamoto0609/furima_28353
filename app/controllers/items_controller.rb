aeaa7aadfce57c38bd80e86b9b686cb81ba36a29
takamoto0609 <takamoto0609@gamil.com>
2020-08-25 11:17:43 +0900

    削除機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,12 @@ class ItemsController < ApplicationController
     redirect_to item_path
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path
+  end
+
   private
 
   def item_params
